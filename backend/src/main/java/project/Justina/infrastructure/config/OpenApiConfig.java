99ee9b520e271b9029bdb746cfb52872afa50e7b
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2026-02-14 20:12:47 -0500

    refactor: se añade documentación en swagger

--- backend/src/main/java/project/Justina/infrastructure/config/OpenApiConfig.java
+++ backend/src/main/java/project/Justina/infrastructure/config/OpenApiConfig.java
@@ -5,11 +5,18 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(title = "Justina API", version = "v1", description = "Plataforma de Telemetría Quirúrgica"),
+
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Servidor Local"),
+                @Server(url = "https://s02-26-equipo-24-web-app-development.onrender.com", description = "Servidor Producción")
+        },
+
         security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
